1b72d9cc34c60468ff8aebf131c4564399a23359
NamKimly <NamKimly>
2024-04-26 00:49:03 +0700

    update project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,86 @@
-import { NavLink } from "react-router-dom";
-export default function Header() {
+import { useState } from "react";
+import { Dialog, Popover } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
 	return (
-		<>
-			<div className="flex justify-between items-center m-12 px-4 ">
-				<div className="header-title ">
-					<h1 className="text-2xl font-bold font-sans"> Electronic Store </h1>
-				</div>
-
-				<div className="header-nav flex justify-center items-center gap-20">
-					<ul className="flex justify-center items-center gap-20 font-medium">
-						<li>
-							<NavLink to="/">Home</NavLink>
-						</li>
-						<li>
-							<NavLink to="/shopping">Brand</NavLink>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-						<li>
-							<a href="#">About</a>
-						</li>
-					</ul>
-
-					<form className="flex items-center max-w-sm mx-auto">
-						<label htmlFor="simple-search" className="sr-only">
-							Search
-						</label>
-						<div className="relative w-full">
-							<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-								<i className="fa-solid fa-magnifying-glass text-white"></i>
-							</div>
-							<input
-								type="text"
-								id="simple-search"
-								className="bg-zinc-300 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-								placeholder="Search..."
-								required
-							/>
+		<header className="navbar bg-white  mb-8">
+			<nav
+				className="mx-12 flex max-w-8xl items-center justify-between p-6 lg:px-5"
+				aria-label="Global">
+				<div className="flex lg:flex-1">
+					<a href="#" className="-m-1.5 p-1.5">
+						<div className="flex justify-center items-center h-1 ">
+							<span className="text-red-600 font-extrabold text-3xl uppercase tracking-wider">
+								Electronic
+							</span>
 						</div>
-					</form>
+					</a>
 				</div>
 
-				<div className="header-right flex justify-center items-center gap-12">
-					<div className="header-cart">
-						<i className="fa-solid fa-cart-shopping cursor-pointer"></i>
+				<div className="flex lg:hidden">
+					<button
+						type="button"
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
+						onClick={() => setMobileMenuOpen(true)}>
+						<span className="sr-only">Open main menu</span>
+						<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+					</button>
+				</div>
+
+				<Popover.Group className="hidden lg:flex lg:gap-x-20  justify-center items-center">
+					<Link to={"/"} className="text-sm font-semibold leading-6 text-black">
+						Home
+					</Link>
+					<Link
+						to={"/shop"}
+						className="text-sm font-semibold leading-6 text-black">
+						Shop
+					</Link>
+					<a href="#" className="text-sm font-semibold leading-6 text-black">
+						About
+					</a>
+					<a href="#" className="text-sm font-semibold leading-6 text-black">
+						Contact
+					</a>
+					<div className="relative">
+						<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+							<svg
+								className="w-4 h-4 text-gray-500 dark:text-gray-400"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 20 20">
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+								/>
+							</svg>
+						</div>
+						<input
+							type="search"
+							id="default-search"
+							className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="Search..."
+							required
+						/>
 					</div>
-
-					<div className="header-login flex justify-center items-center gap-4">
-						<div className="login flex justify-center items-center gap-1">
-							<i className="fa-solid fa-user"></i>
-							<a href="#" className="">
-								Login
-							</a>
-						</div>
+					<div className="flex justify-center items-center gap-12">
+						<a href="#" className="text-sm font-semibold leading-6 text-black">
+							<i className="fa-solid fa-cart-shopping text-lg "></i>
+						</a>
+						<Link
+							to={"/login"}
+							className="text-md leading-6 flex justify-center items-center gap-4 text-black">
+							<i className="fa-solid fa-user text-lg"></i>
+							Log in
+						</Link>
 
 						<button
 							type="button"
@@ -61,8 +88,84 @@ export default function Header() {
 							Sign Up
 						</button>
 					</div>
-				</div>
-			</div>
-		</>
+				</Popover.Group>
+			</nav>
+
+			{/* Mobile View  */}
+
+			<Dialog
+				as="div"
+				className="lg:hidden"
+				open={mobileMenuOpen}
+				onClose={setMobileMenuOpen}>
+				<div className="fixed inset-0 z-10" />
+				<Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+					<div className="flex items-center justify-between">
+						<a href="#" className="-m-1.5 p-1.5">
+							<div className="flex justify-center items-center h-1 bg-black">
+								<span className="text-red-600 font-extrabold text-3xl uppercase tracking-wider">
+									Electronic
+								</span>
+							</div>
+						</a>
+						<button
+							type="button"
+							className="-m-2.5 rounded-md p-2.5 text-white"
+							onClick={() => setMobileMenuOpen(false)}>
+							<span className="sr-only">Close menu</span>
+							<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="mt-6 flow-root">
+						<div className="-my-6 divide-y divide-gray-500/10">
+							<div className="space-y-2 py-6">
+								<div className="flex justify-start items-start gap-4">
+									<i className="fa-solid fa-magnifying-glass text-white absolute p-2.5"></i>
+									<input
+										type="search"
+										id="default-search"
+										className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+										placeholder="Search..."
+										required
+									/>
+								</div>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white ">
+									Home
+								</a>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white ">
+									Brand
+								</a>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white ">
+									About
+								</a>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white ">
+									Contact
+								</a>
+							</div>
+							<div className=" flex justify-start items-start gap-8">
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white">
+									Log in
+								</a>
+								<button
+									type="button"
+									className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+									Sign Up
+								</button>
+							</div>
+						</div>
+					</div>
+				</Dialog.Panel>
+			</Dialog>
+		</header>
 	);
 }
--- src/views/pages/Shopping.jsx
+++ src/views/pages/Shopping.jsx
@@ -5,21 +5,21 @@ console.log(typeof product);
 const SideBar = () => {
 	return (
 		<>
-			<div className="flex flex-col shadow-2xl gap-8 p-8 ml-8">
+			<div className="flex flex-col shadow-2xl gap-8 p-8 ml-8 w-1/6 mb-8">
 				<div className="filter-1 flex flex-col items-start justify-start gap-2">
 					<div>
-						<p className="font-semibold text-xl mb-3"> Category-1</p>
+						<p className="font-semibold text-lg md:text-xl mb-3">Price</p>
 						<input
 							id="default-radio-1"
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
+							Under $20
 						</label>
 					</div>
 
@@ -29,12 +29,12 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
+							$20- 40$
 						</label>
 					</div>
 
@@ -44,12 +44,83 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
+							$50- Above
+						</label>
+					</div>
+
+					<div className="flex justify-start items-start mt-2 ">
+						<div className="flex justify-start items-start w-full h-8">
+							<input
+								type="text"
+								id="min-price"
+								className=" p-1.5  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500  w-3/4  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="Min Price"
+								required
+							/>
+						</div>
+						<div className="flex justify-start items-start w-full h-8">
+							<input
+								type="text"
+								id="max-price"
+								className="p-1.5  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500  w-3/4  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="Max Price"
+								required
+							/>
+						</div>
+					</div>
+				</div>
+				<hr className="container  h-0.5 border-t-0 bg-zinc-300" />
+
+				{/** */}
+				<div className="filter-1 flex flex-col items-start justify-start gap-2">
+					<div>
+						<p className="font-semibold text-xl mb-3"> Product Category</p>
+						<input
+							id="default-radio-1"
+							type="checkbox"
+							value=""
+							name="default-radio"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
+						/>
+						<label
+							htmlFor="default-radio-1"
+							className="ms-2 text-sm font-medium text-black">
+							Small Kitchen Appliance
+						</label>
+					</div>
+
+					<div>
+						<input
+							id="default-radio-1"
+							type="checkbox"
+							value=""
+							name="default-radio"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
+						/>
+						<label
+							htmlFor="default-radio-1"
+							className="ms-2 text-sm font-medium text-black">
+							Cookware
+						</label>
+					</div>
+
+					<div>
+						<input
+							id="default-radio-1"
+							type="checkbox"
+							value=""
+							name="default-radio"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
+						/>
+						<label
+							htmlFor="default-radio-1"
+							className="ms-2 text-sm font-medium text-black">
+							Large Appliance
 						</label>
 					</div>
 					<div>
@@ -58,32 +129,63 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							className="ms-2 text-sm font-medium text-black">
+							Cooking and Heating
+						</label>
+					</div>
+
+					<div>
+						<input
+							id="default-radio-1"
+							type="checkbox"
+							value=""
+							name="default-radio"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
+						/>
+						<label
+							htmlFor="default-radio-1"
+							className="ms-2 text-sm font-medium text-black">
+							Floor care and Garment
+						</label>
+					</div>
+
+					<div>
+						<input
+							id="default-radio-1"
+							type="checkbox"
+							value=""
+							name="default-radio"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
+						/>
+						<label
+							htmlFor="default-radio-1"
+							className="ms-2 text-sm font-medium text-black">
+							LED TV and Audio
 						</label>
 					</div>
 				</div>
+				<hr className="container  h-0.5 border-t-0 bg-zinc-300" />
 
 				{/* */}
 
 				<div className="filter-1 flex flex-col items-start justify-start gap-2">
 					<div>
-						<p className="font-semibold text-xl mb-3"> Category-2</p>
+						<p className="font-semibold text-xl mb-3"> Product Brand</p>
 						<input
 							id="default-radio-1"
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							className="ms-2 text-sm font-medium text-black">
+							SamSung
 						</label>
 					</div>
 
@@ -93,12 +195,12 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							className="ms-2 text-sm font-medium text-black">
+							Sony
 						</label>
 					</div>
 
@@ -108,12 +210,12 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							className="ms-2 text-sm font-medium text-black">
+							LG
 						</label>
 					</div>
 					<div>
@@ -122,32 +224,33 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							className="ms-2 text-sm font-medium text-black">
+							Panasonic
 						</label>
 					</div>
 				</div>
+				<hr className="container  h-0.5 border-t-0 bg-zinc-300" />
 
 				{/* */}
 
 				<div className="filter-1 flex flex-col items-start justify-start gap-2">
 					<div>
-						<p className="font-semibold text-xl mb-3"> Category-3</p>
+						<p className="font-semibold text-xl mb-3"> Deal & Discount</p>
 						<input
 							id="default-radio-1"
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
+							Discount
 						</label>
 					</div>
 
@@ -157,12 +260,12 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
+							Popular
 						</label>
 					</div>
 
@@ -172,93 +275,17 @@ const SideBar = () => {
 							type="checkbox"
 							value=""
 							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 "
 						/>
 						<label
 							htmlFor="default-radio-1"
 							className="ms-2 text-md font-medium text-black">
-							Default radio
-						</label>
-					</div>
-					<div>
-						<input
-							id="default-radio-1"
-							type="checkbox"
-							value=""
-							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
+							New Arrival
 						</label>
 					</div>
 				</div>
 
 				{/* */}
-
-				<div className="filter-1 flex flex-col items-start justify-start gap-2">
-					<div>
-						<p className="font-semibold text-xl mb-3"> Category-4</p>
-						<input
-							id="default-radio-1"
-							type="checkbox"
-							value=""
-							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
-						</label>
-					</div>
-
-					<div>
-						<input
-							id="default-radio-1"
-							type="checkbox"
-							value=""
-							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
-						</label>
-					</div>
-
-					<div>
-						<input
-							id="default-radio-1"
-							type="checkbox"
-							value=""
-							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
-						</label>
-					</div>
-					<div>
-						<input
-							id="default-radio-1"
-							type="checkbox"
-							value=""
-							name="default-radio"
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="default-radio-1"
-							className="ms-2 text-md font-medium text-black">
-							Default radio
-						</label>
-					</div>
-				</div>
 			</div>
 		</>
 	);
@@ -266,17 +293,17 @@ const SideBar = () => {
 
 const ProductsList = ({ product }) => {
 	return (
-		<div className="container flex gap-8">
+		<div className="container mx-auto flex gap-8 flex-wrap max-w-2xl px-2 lg:max-w-7xl">
 			{product.map((product, key) => (
 				<div
 					key={key}
-					className="relative h-full flex flex-col mt- text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl w-64">
-					<div className="flex relative h-52 mx-4 mt-4 overflow-hidden text-white  bg-clip-border rounded-md shadow-blue-gray-500/40">
+					className="relative lg:max-w-7xl   h-full flex flex-col  text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl lg:w-64">
+					<div className="flex relative h-52 mx-4 mt-4 overflow-hidden text-white bg-clip-border rounded-md shadow-blue-gray-500/40">
 						<div className="w-full h-full flex justify-center items-center m-4 ">
 							<img
 								src={product.images[0].image1}
 								alt="card-image"
-								className="max-w-full h-3/4 hover:scale-105 cursor-pointer"
+								className="max-w-48 object-cover object-center lg:h-3/4 transition-transform duration-300 transform hover:scale-105 cursor-pointer"
 							/>
 						</div>
 
@@ -290,9 +317,9 @@ const ProductsList = ({ product }) => {
 					</div>
 
 					<div className="p-6">
-						<h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+						<p className="block mb-2 font-sans lg:text-xl md:text-base antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
 							{product.product_name}
-						</h5>
+						</p>
 						<p className="block font-sans text-base text-red-500 antialiased font-light leading-relaxed text-inherit">
 							${product.price}
 						</p>
@@ -304,8 +331,8 @@ const ProductsList = ({ product }) => {
 							Add To Cart
 						</button>
 						<div className="flex justify-center items-center gap-4">
-							<i className="far fa-heart cursor-pointer"></i>
-							<i className="fas fa-eye text-black cursor-pointer"></i>
+							<i className="far fa-heart cursor-pointer md:text-sm"></i>
+							<i className="fas fa-eye text-black cursor-pointer md:text-sm"></i>
 						</div>
 					</div>
 				</div>
@@ -317,7 +344,7 @@ const ProductsList = ({ product }) => {
 export default function Shopping() {
 	return (
 		<>
-			<div className="flex gap-32">
+			<div className="flex gap-20">
 				<SideBar />
 				<div className="flex justify-start items-start">
 					<ProductsList product={product} />
